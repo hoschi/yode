e970aadb3df34212594e609f8b2c9ccf579edea5
Stefan Gojan <vollekannehoschi@gmail.com>
2017-04-13 15:41:03 +0200

    add link to github project page

--- src/js/components/Shell.js
+++ src/js/components/Shell.js
@@ -3,11 +3,17 @@ import muiThemeConfig from 'muiThemeConfig';
 import AppBar from 'material-ui/AppBar'
 import Paper from 'material-ui/Paper'
 import ControlsContainer from './ControlsContainer'
+import githubMarkImg from 'file-loader!src/assets/GitHub-Mark.png';
 
 const Shell = ({children}) => {
     return <div>
                <Paper style={ style.pinnedArea } zDepth={ 2 }>
-                   <AppBar title="NFBE Prototype" showMenuIconButton={ false } zDepth={ 0 } />
+                   <div style={ style.header }>
+                       <AppBar title="NFBE Prototype" showMenuIconButton={ false } zDepth={ 0 } />
+                        <a href="https://github.com/hoschi/nfbe" title="Github project page">
+                            <img style={style.githubImg} src={githubMarkImg} alt="github mark"/>
+                        </a>
+                   </div>
                    <ControlsContainer />
                </Paper>
                <div style={ style.mainArea }>
@@ -31,6 +37,15 @@ let style;
         },
         mainArea: {
             marginTop: pinnedAreaHeight
+        },
+        header: {
+            display: 'flex',
+        },
+        githubImg:{
+            height:24,
+            width:24,
+            margin:20,
+            marginRight:24
         }
     }
 }
