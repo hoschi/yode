016ecd89b1bc2914cffe081faf1726b308036b21
Stefan Gojan <vollekannehoschi@gmail.com>
2017-08-26 14:53:05 +0200

    change public path for build

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -3,5 +3,8 @@
 var base = require('./webpack.config.base');
 
 module.exports = Object.assign({}, base, {
+    output: Object.assign({}, base.output, {
+        publicPath: '/yode/'
+    }),
     devtool: 'source-map'
 })
