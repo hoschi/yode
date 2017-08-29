96535df8cbff2ef92ee66bac5e2fe20fd69ba3dd
Stefan Gojan <vollekannehoschi@gmail.com>
2017-08-29 11:13:50 +0200

    #1 remove test code

--- packages/core/src/api.js
+++
@@ -1,5 +0,0 @@
-export default {
-    magic() {
-        console.log('it is magic')
-    }
-}
--- packages/demo/src/components/Controls.js
+++ packages/demo/src/components/Controls.js
@@ -2,10 +2,8 @@ import React from 'react'
 import RaisedButtonWithTooltip from './RaisedButtonWithTooltip'
 import OpenFileMenuContainer from './OpenFileMenuContainer'
 import AddFileDialogContainer from './AddFileDialogContainer'
-import core, {otherTest} from 'yode-core'
-import api from 'yode-core/src/api'
 
-const Controls = ({onFormatClick/*, onOpenFunctionEditorUnderCursorClick, openFunctionEditorDisabled*/}) => {
+const Controls = ({onFormatClick, onOpenFunctionEditorUnderCursorClick, openFunctionEditorDisabled}) => {
     let formatButtonProps = {
         label: 'format code',
         style: {
@@ -16,27 +14,12 @@ const Controls = ({onFormatClick/*, onOpenFunctionEditorUnderCursorClick, openFu
         tooltip: 'Recast formats code mostly as you type it',
         onTouchTap: onFormatClick
     }
-    /*
-     *let openFunctionEditorButtonProps = {
-     *    label: 'open function editor',
-     *    style: buttonStyle,
-     *    onTouchTap: onOpenFunctionEditorUnderCursorClick,
-     *    disabled: openFunctionEditorDisabled,
-     *    tooltip: 'opens function editor for function under cursor in focused editor'
-     *}
-     */
-    // FIXME replace with above when module tests finished
     let openFunctionEditorButtonProps = {
-        label: 'my test button',
+        label: 'open function editor',
         style: buttonStyle,
-        onTouchTap: () => {
-            core('foo');
-            otherTest({
-                bar:'baaaaaaaar'
-            })
-            api.magic();
-        },
-        tooltip: 'do it'
+        onTouchTap: onOpenFunctionEditorUnderCursorClick,
+        disabled: openFunctionEditorDisabled,
+        tooltip: 'opens function editor for function under cursor in focused editor'
     }
     return <div style={ containerStyle }>
                <RaisedButtonWithTooltip {...openFunctionEditorButtonProps} />
