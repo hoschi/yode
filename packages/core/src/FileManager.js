270731fc53ba83a329c6feab99f2436827dd0612
Stefan Gojan <vollekannehoschi@gmail.com>
2017-09-09 10:29:23 +0200

    #1 - save function nodes in storage

--- packages/core/src/FileManager.js
+++ packages/core/src/FileManager.js
@@ -2,6 +2,7 @@ import stampit from '@stamp/it';
 import R from 'ramda'
 import profiler from './profiler'
 import File from './File'
+import FunctionBuffer from './FunctionBuffer'
 
 function initInputFile (inputFile) {
     const {id, text} = inputFile;
@@ -15,46 +16,67 @@ function initInputFile (inputFile) {
 
 // FIXME this is a buffer manager as it also stores function buffers to match buffer id from editor to function and its file
 let FileManager = stampit().deepProps({
-    storage: undefined,
+    files: undefined,
+    functionBuffers: undefined,
     editorApi: undefined
 }).methods({
     init(editorApi, inputFiles = []) {
         this.editorApi = editorApi
-        // FIXME process files first, then try to find other buffers in that files
         let files = inputFiles.map(initInputFile)
-        this.storage = R.zipObj(files.map(R.prop('id')), files)
+        this.files = R.zipObj(files.map(R.prop('id')), files)
+
+        this.functionBuffers = {}
     },
     addFile(inputFile) {
         let file = initInputFile(inputFile)
-        this.storage[file.id] = file;
+        this.files[file.id] = file;
+    },
+    getFileAndNodeForBufferId(bufferId) {
+        let file, node
+        if (this.functionBuffers[bufferId]) {
+            let buffer = this.functionBuffers[bufferId]
+            file = this.files[buffer.fileId]
+            node = R.find(R.propEq('customId', buffer.customId), file.functions)
+        } else {
+            file = this.files[bufferId]
+            node = file.ast;
+        }
+        return { file, node }
     },
     openFunctionUnderCursor(bufferId, cursor) {
-        let stop = profiler.start('- open function editor for function under cursor')
-        let file = this.storage[bufferId]
-        // FIXME this can be also function node, if bufferId represents function editor
-        let node = file.ast;
+        const stop = profiler.start('- open function editor for function under cursor')
+
+        const {file, node} = this.getFileAndNodeForBufferId(bufferId)
+
         if (!file || !node) {
             // can't find file or node for (perhaps not) focused editor
             stop()
         }
 
         let foundFunction = file.findFunctionAroundCursor(node, cursor)
+
+        if (!foundFunction) {
+            // nothing found
+            stop()
+            return
+        }
+
         if (foundFunction.customId === node.customId) {
             // inner most function is the same function as the editor already focused, nothing to do
             stop()
-            // FIXME do it for function editors
-            console.log('not implemented');
-        } else if (foundFunction !== undefined) {
-            // open editor
+        } else {
+            // Yode is done, tell editor to open found node
             stop()
 
             // FIXME check if buffer exists and focus it instead of creating it again
             let bufferId = this.editorApi.createBuffer(foundFunction.unformattedText)
-            // FIXME add function editor to storage and also associate created function editor (buffer) with internal storage, we need this later when we want a function and bufferId stands for function editor
+            let functionBuffer = FunctionBuffer.create()
+            functionBuffer.init({
+                customId:foundFunction.customId,
+                fileId:file.id
+            })
+            this.functionBuffers[bufferId] = functionBuffer
             this.editorApi.openBuffer(bufferId)
-        } else {
-            // nothing found
-            stop()
         }
     }
 })
