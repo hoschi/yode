08b958e68978e0be709b2b0477b09c566e88c18b
Stefan Gojan <vollekannehoschi@gmail.com>
2017-12-27 16:06:40 +0100

    unit test for File

--- packages/core/src/testCodeSnippets.js
+++ packages/core/src/testCodeSnippets.js
@@ -2,6 +2,19 @@ export let letOnly = `
 let only = 'simple'
 `.trim()
 
+// trailing space after name and before args parens
+export let recastFormat = `
+function add (a, b) {
+    return a + b;
+}
+`.trim()
+
+export let recastFormat_formatted = `
+function add(a, b) {
+    return a + b;
+}
+`.trim()
+
 export let nestedFunctions = `
 let myVar = 'some string'
 function myFunc (a) {
@@ -52,6 +65,88 @@ import React from 'react'
 let HelloComponent = ({name}) => {
     return <span>Hello&nbsp;{name}!</span>;
 }
+`.trim()
+
+export let jsxComponentAndTest_withError = `
+import React from 'react'
+
+let HelloComponent = ({name}) => {
+    return <span>Hello&nbsp;{name}!</span>;
+}
+
+describe('HelloComponent', () => {
+    // ERROR HERE, sting not closed
+    let name = 'Walter
+
+    it('displays name', () => {
+        function getTestName(a, b) {
+            return a + b + name;
+        }
+
+        let myName = getTestName('I', 'Am', name);
+        expect(<HelloComponent name={myName} />).contain('Hello IAmWalter!')
+    })
+
+    // function not recognized because not of a supported type
+    it("doesn't work without name", function () {
+        let myName = undefined;
+        expect(<HelloComponent name={myName} />).contain('Hello !')
+    });
+})
+`.trim()
+
+export let jsxComponentAndTest_changedInnerMostFunction = `
+import React from 'react'
+
+let HelloComponent = ({name}) => {
+    return <span>Hello&nbsp;{name}!</span>;
+}
+
+describe('HelloComponent', () => {
+    let name = 'Walter';
+
+    it('displays name', () => {
+        function getTestName(a, b) {
+            return a + b + name+'mytest';
+        }
+
+        let myName = getTestName('I', 'Am', name);
+        expect(<HelloComponent name={myName} />).contain('Hello IAmWalter!')
+    })
+
+    // function not recognized because not of a supported type
+    it("doesn't work without name", function () {
+        let myName = undefined;
+        expect(<HelloComponent name={myName} />).contain('Hello !')
+    });
+})
+`.trim()
+
+export let jsxComponentAndTest_changedOuterFunction = `
+import React from 'react'
+
+let HelloComponent = ({name}) => {
+    return <span>Hello&nbsp;{name}!</span>;
+}
+
+describe('HelloComponent', () => {
+    let name = 'Walterrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr'
+
+    it('displays name', () => {
+        function getTestName(a, b) {
+            return a + b + name;
+        }
+
+        let myName = getTestName('I', 'Am', name);
+        expect(<HelloComponent name={myName} />).contain('Hello IAmWalter!')
+    })
+
+    // function not recognized because not of a supported type
+    it("doesn't work without name", function () {
+        let myName = undefined;
+        expect(<HelloComponent name={myName} />).contain('Hello !')
+    });
+})
 `.trim()
 
 export let jsxClassesAndObjects = `
